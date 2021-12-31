19bffca8c273b2bb85a86e404a85f1ef505663b0
AnthonyStembreit <stembreit@gmail.com>
2021-12-30 22:08:10 -0600

    presenting skills on react client

--- client/src/pages/innerPages/publicProfile/index.js
+++ client/src/pages/innerPages/publicProfile/index.js
@@ -1,10 +1,26 @@
 import { useState, useEffect, React } from 'react';
 import { Col, Row, Container, Carousel } from 'react-bootstrap';
 import CommunityCard from '../../../components/innerComponents/communityCard';
-import { useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 import './profile.css';
+import axios from 'axios';
 export default function PublicProfile() {
     const state = useSelector(state => state);
+    const [stateSkills, setSkills] = useState([])
+    useEffect(() => {
+        axios.get(`/api/skill/${state.user.id}`).then((res) => {
+            console.log(res)
+        let skills = res.data.map(skill => {
+                return (
+                    <div value={skill.id} className='flex-skill'>
+                        <p>{skill.name}</p>
+                        <p>{skill.currentLevel}</p>
+                    </div>
+                )
+            })
+            setSkills(skills)
+        })
+    }, [])
     const props = {
         skills: [
             {
@@ -89,66 +105,66 @@ export default function PublicProfile() {
 
         )
     });
-    let skills = props.skills.map(skill =>{
-        return (
-            <div className='flex-skill'>
-                <p>{skill.name}</p>
-                <p>{skill.currentLevel}</p>
-            </div>
-        )
-    })
-const [stateRows, setRows] = useState([communities])
-useEffect(()=>{
-    console.log(communities.length);
-    let rows = [];
-    for (let i = 0; i < communities.length; i += 3) {
-        console.log(i)
-        rows.push(
-            <Carousel.Item>
-                <Row>
-                    {communities[i]}
-                    {communities[i + 1]}
-                    {communities[i + 2]}
-                </Row>
-            </Carousel.Item>
-        )
-    };
-    setRows(rows)
-    console.log(stateRows);
-}, [])
-console.log(state)
-console.log(state.user)
-const {img_url, description, username, link_name_one, link_url_one,link_name_two, link_url_two, link_name_three, link_url_three} = state.user
+    // let skills = props.skills.map(skill => {
+    //     return (
+    //         <div className='flex-skill'>
+    //             <p>{skill.name}</p>
+    //             <p>{skill.currentLevel}</p>
+    //         </div>
+    //     )
+    // })
+    const [stateRows, setRows] = useState([communities])
+    useEffect(() => {
+        console.log(communities.length);
+        let rows = [];
+        for (let i = 0; i < communities.length; i += 3) {
+            console.log(i)
+            rows.push(
+                <Carousel.Item>
+                    <Row>
+                        {communities[i]}
+                        {communities[i + 1]}
+                        {communities[i + 2]}
+                    </Row>
+                </Carousel.Item>
+            )
+        };
+        setRows(rows)
+        console.log(stateRows);
+    }, [])
+    console.log(state)
+    console.log(state.user)
+    const { img_url, description, username, link_name_one, link_url_one, link_name_two, link_url_two, link_name_three, link_url_three } = state.user
     return (
         <Container id="public-profile">
             <Row id="info-row" className="outer-rows">
-          
+
                 <Col className="top-row-col">
                     <Row id="img-row"><img src={img_url} alt="profile picture"></img></Row>
                     <Row><ul>
-                    <li><a href={link_url_one}>{link_name_one}</a></li>
-                    <li><a href={link_url_two}>{link_name_two}</a></li>
-                    <li><a href={link_url_three}>{link_name_three}</a></li>
-                        </ul></Row>
+                        <li><a href={link_url_one}>{link_name_one}</a></li>
+                        <li><a href={link_url_two}>{link_name_two}</a></li>
+                        <li><a href={link_url_three}>{link_name_three}</a></li>
+                    </ul></Row>
                 </Col>
                 <Col className="top-row-col">
                     <Row><h2>{username}</h2></Row>
-                    <Row id="skill-box" > {skills}</Row>
+                    <Row id="skill-box" > {stateSkills}</Row>
                 </Col>
-               
+
             </Row>
             <Row className="outer-rows">
-        
+
                 <Col lg={10}>{description}</Col>
 
             </Row>
             <Row className="outer-rows">
-          
+
                 <Col id="skill-box" lg={10}></Col>
-            
+
             </Row>
             <Row className="outer-rows">
-                
+
                 <Col lg={10}>
                     <Carousel>
                         {stateRows}
